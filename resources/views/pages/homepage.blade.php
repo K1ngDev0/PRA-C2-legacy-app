ef01663c4db919b3e9c5b4fc4903990cca6b913a
K1ngDev0 <zijlmanslars@gmail.com>
2024-10-09 09:42:44 +0200

    top merken en handleidingen
    We hebben samen toegevoegd dat op de homepagina de 5 populairste merken gezien worden. Ook staan de 10 populairste handleidingen erop.

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -3,8 +3,31 @@
     <h1>Welkom, {{ $name }}!</h1>
     <br>
 
-    <h1>All Categories</h1>
+    <h2>Populaire Merken</h2>
+    <ul>
+        @if(!empty($popularBrands) && $popularBrands->count() > 0)
+            @foreach($popularBrands as $brand)
+                <li>{{ $brand->name }} - {{ $brand->visit_count }} visits</li> <!-- Weergeven als [type] -->
+            @endforeach
+        @else
+            <li>Er zijn geen populaire handleidingen beschikbaar.</li>
+        @endif
+    </ul>
 
+    <h2>Populaire Handleidingen</h2>
+    <ul>
+        @if(!empty($popularManuals) && $popularManuals->count() > 0)
+            @foreach($popularManuals as $manual)
+                <li>{{ $manual->name }} - {{ $manual->visit_count }} visits</li> <!-- Weergeven als [type] -->
+            @endforeach
+        @else
+            <li>Er zijn geen populaire handleidingen beschikbaar.</li>
+        @endif
+    </ul>
+    
+    
+
+    <h1>All Categories</h1>
 
     <div class="container">
         <div class="row">
